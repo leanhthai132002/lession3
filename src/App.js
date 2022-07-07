0b40bdd35f5c0f5c16d234c61868b912e2f4ea1c
LeAnhThai <vanuong132002@gmail.com>
2022-07-07 09:02:51 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 import Square from './components/square';
 import styled from 'styled-components'
+import React,{useState} from 'react';
 function App() {
+  const[board, setBoard] = useState(Array(9).fill(null))
+  const [xPlayer, setXPlayer] =  useState(true)
+  const handlePlay = (index)=>{
+const temp = board.slice()
+if(xPlayer){
+  temp[index] = "X"
+}else{
+  temp[index] = "O"
+}
+setBoard(temp)
+setXPlayer(!xPlayer)
+  }
+  const winner = caculateWinner(board)
   return (
+    
+  <div>
+     {winner ? winner :null}
     <Board>
-  <Square/>
-  <Square/>
-  <Square/>
-  <Square/>
-  <Square/>
-  <Square/>
-  <Square/>
-  <Square/>
-  <Square/>
+ {board.map((item, index) => <Square value={item} handlePlay={()=> handlePlay(index)} key={index}/>)}
     </Board>
+  </div>
   );
 }
-
-function Box(props) {
-  console.log(props);
-  return (
-    <div style={{
-      margin:"auto",
-      backgroundColor: props.color,
-      height: 100,
-      with: 100
-    }}>
-      <Text  value="This is the box" textColor={props.textColor}/>
-    </div>
-  )
-}
-
-function Text(props) {
-  return <h3 style={{color: props.textColor}}>{props.value}</h3>
-}
-
 const Board = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 5px;
   margin: auto;
-  height: 300px;
-  width: 300px;
-  margin-top: 10px;
+width: 300px;
+height: 300px;
+  margin-top: 40px;
+  padding: 10px;
 `
-
+const  caculateWinner =(board) =>{
+  const winLine = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+  for(let i = 0; i < winLine.length; i++){
+const [a,b,c] = winLine[i]
+if(board[a] && board[b] && board[a] == board[c]){
+return board[a]
+}
+  }
+  return null
+}
 export default App;
--- src/components/square/index.js
+++ src/components/square/index.js
@@ -1,11 +1,8 @@
 import React, {useState} from 'react'
 import styles from './square.module.css'
 
-const Square = () => {
-    const [status, setStatus] = useState(false)
-    return <button onClick={() => setStatus(!status)} className={styles.square} >
-        {status ? "X" : null}
-    </button>
+const Square = ({value, handlePlay}) => {
+    return <button className={styles.square} onClick={handlePlay}> {value} </button>
 }
 
-export default Square
+export default Square;
